96ab6a7c8578c832f44caa374a951bdebaa9ee1f
Rocio Garrido <rocio.garrido@wefox.com>
2021-12-01 11:51:03 +0100

    first commit fizzbuzz kata

--- fizzbuzz/src/fizzbuzz.js
+++ fizzbuzz/src/fizzbuzz.js
@@ -1,3 +1,17 @@
 export default () => {
-  return true;
+  const elList = [];
+
+  for (let x=1; x<=100; x++) {
+    if ((x % 3 ) === 0) {
+      elList.push("Fizz")
+    }
+    else if ((x % 5 ) === 0) {
+      elList.push("Buzz")
+    }
+    else {
+      elList.push(x)
+    }
+  }
+  console.log('elList: ', elList);
+  return elList;
 }
--- fizzbuzz/src/fizzbuzz.test.js
+++ fizzbuzz/src/fizzbuzz.test.js
@@ -1,5 +1,13 @@
 import fizzbuzz from "./fizzbuzz";
 
 test("canary test", () => {
-  expect(fizzbuzz()).toEqual(true);
+  expect(fizzbuzz().length).toEqual(100);
+});
+
+test("should print Fizz", () => {
+  expect(fizzbuzz()[2]).toEqual("Fizz");
+});
+
+test("should print Buzz", () => {
+  expect(fizzbuzz()[4]).toEqual("Buzz");
 });
