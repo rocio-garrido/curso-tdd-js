99991c90468460cb585533b95d2b8a8ed343f5da
Rocio Garrido <rocio.garrido@wefox.com>
2021-12-02 09:07:33 +0100

    add gilded rose kata

--- fizzbuzz/src/fizzbuzz.js
+++ fizzbuzz/src/fizzbuzz.js
@@ -1,18 +1,18 @@
 export default () => {
   const elList = [];
 
-  for (let x=1; x<=100; x++) {
-    if ((x % 3 === 0) && (x % 5 === 0)) {
+  for (let number = 1; number <= 100; number++) {
+    if ((number % 3 === 0) && (number % 5 === 0)) {
       elList.push("FizzBuzz")
     }
-    else if ((x % 3 ) === 0) {
+    else if ((number % 3 ) === 0) {
       elList.push("Fizz")
     }
-    else if ((x % 5 ) === 0) {
+    else if ((number % 5 ) === 0) {
       elList.push("Buzz")
     }
     else {
-      elList.push(x)
+      elList.push(number)
     }
   }
   console.log('elList: ', elList);
--- gilded-rose/src/Shop.test.js
+++ gilded-rose/src/Shop.test.js
@@ -1,11 +1,58 @@
 import Shop from "./Shop";
 import Item from "./Item";
 
-test("canary test", () => {
-  const item = new Item("foo", 1, 3);
+const MIN_QUALITY = 0;
+const MAX_QUALITY = 50;
+
+function regularItem({sellIn, quality}) {
+  return new Item("foo", sellIn, quality);
+}
+
+function agedBrieItem({sellIn, quality}) {
+  return new Item("Aged Brie", sellIn, quality);
+}
+
+test("quality decreases every day", () => {
+  const item = regularItem({ sellIn: 1, quality: 3 });
   const shop = new Shop([item]);
 
   shop.updateQuality();
 
   expect(item.quality).toEqual(2);
+});
+
+test("sellIn value decreases every day", () => {
+  const item = regularItem({ sellIn: 1, quality: 3 });
+  const shop = new Shop([item]);
+
+  shop.updateQuality();
+
+  expect(item.sellIn).toEqual(0);
+});
+
+test("once the sell by date has passed, quality degrades twice as fast", () => {
+  const item = regularItem({ sellIn: 0, quality: 3 });
+  const shop = new Shop([item]);
+
+  shop.updateQuality();
+
+  expect(item.quality).toEqual(1);
+});
+
+test("quality of an item is never negative", () => {
+  const item = regularItem({ sellIn: 5, quality: 0 });
+  const shop = new Shop([item]);
+
+  shop.updateQuality();
+
+  expect(item.quality).toEqual(MIN_QUALITY);
+});
+
+test("aged brie increases in quality", () => {
+  const item = agedBrieItem({ sellIn: 5, quality: 0 });
+  const shop = new Shop([item]);
+
+  shop.updateQuality();
+
+  expect(item.quality).toEqual(1);
 });
